608bbac29e8a5ecf81c308f147ecda52f418ae89
apotasu <apotasu23@gmail.com>
2024-08-14 17:56:42 -0300

    fix: arrumado a relação entre pauta e voto

--- Desafio/src/main/java/desafiovotos/pautas/entity/Pauta.java
+++ Desafio/src/main/java/desafiovotos/pautas/entity/Pauta.java
@@ -1,12 +1,17 @@
 package desafiovotos.pautas.entity;
 
 import desafiovotos.votos.entity.Voto;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 
 import java.sql.Time;
 import java.util.ArrayList;
 
+@Entity
 public class Pauta {
     @Id
     @GeneratedValue
@@ -14,7 +19,9 @@ public class Pauta {
 
     private String descricao;
     private Time duracao;
+    @OneToMany
     private ArrayList<Voto> votos;
+    private boolean decicaoFinal;
 
     public Pauta(String descricao, Time duracao) {
         this.descricao = descricao;
@@ -24,6 +31,14 @@ public class Pauta {
     public Pauta(String descricao) {
         this.descricao = descricao;
         this.duracao = Time.valueOf("60");
+    }
+
+    public Pauta() {
+
+    }
+
+    public boolean isDecicaoFinal() {
+        return decicaoFinal;
     }
 
     public void setDescricao(String descricao){
--- Desafio/src/main/java/desafiovotos/votos/entity/Voto.java
+++ Desafio/src/main/java/desafiovotos/votos/entity/Voto.java
@@ -1,10 +1,12 @@
 package desafiovotos.votos.entity;
 
+import desafiovotos.pautas.entity.Pauta;
 import desafiovotos.pessoa.entity.Pessoa;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
 
 @Entity
@@ -15,6 +17,9 @@ public class Voto {
     @OneToOne
     @JoinColumn(name = "pessoa_id")
     private Pessoa pessoa;
+
+    @ManyToOne
+    private Pauta pauta;
 
     private boolean escolha;
 
